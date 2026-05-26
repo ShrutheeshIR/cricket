5b31adf0dafd8937924eee1791b62d076f584614
andyhl-96 <andyhliu96@gmail.com>
2026-05-25 22:46:22 -0600

    add code for pinn gen

--- src/topple_load.cc
+++ src/topple_load.cc
@@ -38,10 +38,10 @@ std::vector<ADCG> assign_line_to_matrix(std::ifstream& file) {
 
     std::vector<ADCG> row_data;
     while (std::getline(ss, cell, ',')){
-        std::cout << cell;
+        // std::cout << cell;
         row_data.push_back(ADCG(std::stod(cell)));
     }
-    std::cout << " --> " << row_data.size() << std::endl;
+    // std::cout << " --> " << row_data.size() << std::endl;
 
     return row_data;
 }
@@ -57,34 +57,108 @@ struct ToppleNN
       std::ifstream file(weights_path);
 
       // each matrix is stored in a single line
-        
+    
+      const int hsize = 1024;
+      const int deg = 7;
+
+      std::cout << "Read layers from file" << std::endl;
       auto rdata = assign_line_to_matrix(file);
-      ADMatrixXs layer_1_w =  Eigen::Map<ADMatrixXs>(rdata.data(), 6 * dof, 32);
+      ADMatrixXs inp_layer =  Eigen::Map<ADMatrixXs>(rdata.data(), 6 * dof, hsize);
+      rdata = assign_line_to_matrix(file);
+      ADMatrixXs inp_b =  Eigen::Map<ADMatrixXs>(rdata.data(), 1, hsize);
 
       rdata = assign_line_to_matrix(file);
-      ADMatrixXs layer_1_b =  Eigen::Map<ADMatrixXs>(rdata.data(), 1, 32);
-
-    //   rdata = assign_line_to_matrix(file);
-    //   ADMatrixXs layer_2_w =  Eigen::Map<ADMatrixXs>(rdata.data(), 128, 128);
-    //   rdata = assign_line_to_matrix(file);
-    //   ADMatrixXs layer_2_b =  Eigen::Map<ADMatrixXs>(rdata.data(), 1, 128);
+      ADMatrixXs norm_0_w =  Eigen::Map<ADMatrixXs>(rdata.data(), 1, hsize);
+      rdata = assign_line_to_matrix(file);
+      ADMatrixXs norm_0_b =  Eigen::Map<ADMatrixXs>(rdata.data(), 1, hsize);
 
       rdata = assign_line_to_matrix(file);
-      ADMatrixXs layer_3_w =  Eigen::Map<ADMatrixXs>(rdata.data(), 32, 6 * dof + 1);
+      ADMatrixXs linear_1_w =  Eigen::Map<ADMatrixXs>(rdata.data(), hsize, hsize);
       rdata = assign_line_to_matrix(file);
-      ADMatrixXs layer_3_b =  Eigen::Map<ADMatrixXs>(rdata.data(), 1, 6 * dof + 1);
+      ADMatrixXs linear_1_b =  Eigen::Map<ADMatrixXs>(rdata.data(), 1, hsize);
+
+      rdata = assign_line_to_matrix(file);
+      ADMatrixXs norm_1_w =  Eigen::Map<ADMatrixXs>(rdata.data(), 1, hsize);
+      rdata = assign_line_to_matrix(file);
+      ADMatrixXs norm_1_b =  Eigen::Map<ADMatrixXs>(rdata.data(), 1, hsize);
+
+      rdata = assign_line_to_matrix(file);
+      ADMatrixXs linear_2_w =  Eigen::Map<ADMatrixXs>(rdata.data(), hsize, hsize);
+      rdata = assign_line_to_matrix(file);
+      ADMatrixXs linear_2_b =  Eigen::Map<ADMatrixXs>(rdata.data(), 1, hsize);
+
+      rdata = assign_line_to_matrix(file);
+      ADMatrixXs norm_2_w =  Eigen::Map<ADMatrixXs>(rdata.data(), 1, hsize);
+      rdata = assign_line_to_matrix(file);
+      ADMatrixXs norm_2_b =  Eigen::Map<ADMatrixXs>(rdata.data(), 1, hsize);
+
+      rdata = assign_line_to_matrix(file);
+      ADMatrixXs linear_3_w =  Eigen::Map<ADMatrixXs>(rdata.data(), hsize, hsize);
+      rdata = assign_line_to_matrix(file);
+      ADMatrixXs linear_3_b =  Eigen::Map<ADMatrixXs>(rdata.data(), 1, hsize);
+
+      rdata = assign_line_to_matrix(file);
+      ADMatrixXs norm_3_w =  Eigen::Map<ADMatrixXs>(rdata.data(), 1, hsize);
+      rdata = assign_line_to_matrix(file);
+      ADMatrixXs norm_3_b =  Eigen::Map<ADMatrixXs>(rdata.data(), 1, hsize);
+
+      rdata = assign_line_to_matrix(file);
+      ADMatrixXs linear_4_w =  Eigen::Map<ADMatrixXs>(rdata.data(), hsize, hsize);
+      rdata = assign_line_to_matrix(file);
+      ADMatrixXs linear_4_b =  Eigen::Map<ADMatrixXs>(rdata.data(), 1, hsize);
+
+      rdata = assign_line_to_matrix(file);
+      ADMatrixXs norm_4_w =  Eigen::Map<ADMatrixXs>(rdata.data(), 1, hsize);
+      rdata = assign_line_to_matrix(file);
+      ADMatrixXs norm_4_b =  Eigen::Map<ADMatrixXs>(rdata.data(), 1, hsize);
+
+      rdata = assign_line_to_matrix(file);
+      ADMatrixXs linear_5_w =  Eigen::Map<ADMatrixXs>(rdata.data(), hsize, hsize);
+      rdata = assign_line_to_matrix(file);
+      ADMatrixXs linear_5_b =  Eigen::Map<ADMatrixXs>(rdata.data(), 1, hsize);
+
+      rdata = assign_line_to_matrix(file);
+      ADMatrixXs norm_5_w =  Eigen::Map<ADMatrixXs>(rdata.data(), 1, hsize);
+      rdata = assign_line_to_matrix(file);
+      ADMatrixXs norm_5_b =  Eigen::Map<ADMatrixXs>(rdata.data(), 1, hsize);
+
+      rdata = assign_line_to_matrix(file);
+      ADMatrixXs out_layer =  Eigen::Map<ADMatrixXs>(rdata.data(), hsize, dof * (deg - 1) + 1);
+      rdata = assign_line_to_matrix(file);
+      ADMatrixXs out_b =  Eigen::Map<ADMatrixXs>(rdata.data(), 1, dof * (deg - 1) + 1);
+    // std::cout << "reached" << std::endl;
 
       // read from file
-      layer_weights.push_back(layer_1_w);
-    //   layer_weights.push_back(layer_2_w);
-      layer_weights.push_back(layer_3_w);
+      layer_weights.push_back(inp_layer);
+      layer_weights.push_back(norm_0_w);
+      layer_weights.push_back(linear_1_w);
+      layer_weights.push_back(norm_1_w);
+      layer_weights.push_back(linear_2_w);
+      layer_weights.push_back(norm_2_w);
+      layer_weights.push_back(linear_3_w);
+      layer_weights.push_back(norm_3_w);
+      layer_weights.push_back(linear_4_w);
+      layer_weights.push_back(norm_4_w);
+      layer_weights.push_back(linear_5_w);
+      layer_weights.push_back(norm_5_w);
+      layer_weights.push_back(out_layer);
 
-      layer_biases.push_back(layer_1_b);
-    //   layer_biases.push_back(layer_2_b);
-      layer_biases.push_back(layer_3_b);
+      layer_biases.push_back(inp_b);
+      layer_biases.push_back(norm_0_b);
+      layer_biases.push_back(linear_1_b);
+      layer_biases.push_back(norm_1_b);
+      layer_biases.push_back(linear_2_b);
+      layer_biases.push_back(norm_2_b);
+      layer_biases.push_back(linear_3_b);
+      layer_biases.push_back(norm_3_b);
+      layer_biases.push_back(linear_4_b);
+      layer_biases.push_back(norm_4_b);
+      layer_biases.push_back(linear_5_b);
+      layer_biases.push_back(norm_5_b);
+      layer_biases.push_back(out_b);
 
       inp_size = 6 * dof;
-      out_size = 6 * dof + 1;
+      out_size = dof * (deg - 1) + 1;
       std::cout << "Completed construction with " << dof << std::endl;
 
     }
@@ -96,9 +170,6 @@ struct ToppleNN
 
 };
 
-
-
-
 struct Traced
 {
     std::string code;
@@ -106,11 +177,19 @@ struct Traced
     std::size_t outputs;
 };
 
+auto layer_norm(ADVectorXs z) {
+    const float eps = 0.00001;
+    auto mean = z.mean();
+    auto variance = (z.array() - mean).square().mean();
+    auto normalized = (z.array() - mean) / CppAD::sqrt(variance + eps);
+    return normalized.matrix();
+}
+
 auto trace_forward(
     const ToppleNN &fnn_module
 )
 {
-
+    // Inputs are assumed normalized to zscore
     ADVectorXs ad_x(fnn_module.inp_size);
     for (auto i = 0U; i < fnn_module.inp_size; ++i)
         ad_x[i] = ADCG(0.0);
@@ -122,22 +201,91 @@ auto trace_forward(
     // for (auto i = 0U; i < fnn_module.inp_size; ++i)
     //     ix[i] = ad_x[i];
 
-    // excuse this disgustingness
+    const int hsize = 1024;
+    const int dof = 7;
+    const int deg = 7;
+
+    // Excuse this disgustingness, i really just dont want to think about this right now
     auto x = ad_x.transpose();
     std::cout << x.rows() << ", " << x.cols() << std::endl;
     ADCG zero(0.0);
 
-    ADVectorXs ad_h1(32);
-    auto h1 = ad_h1.transpose();
+    ADVectorXs ad_l1(hsize);
+    auto l1 = ad_l1.transpose();
 
-    ADVectorXs ad_y(36);
+    ADVectorXs ad_l2(hsize);
+    auto l2 = ad_l2.transpose();
+
+    ADVectorXs ad_l3(hsize);
+    auto l3 = ad_l3.transpose();
+
+    ADVectorXs ad_l4(hsize);
+    auto l4 = ad_l4.transpose();
+
+    ADVectorXs ad_l5(hsize);
+    auto l5 = ad_l5.transpose();
+
+    ADVectorXs ad_y(dof * (deg - 1) + 1);
     auto y = ad_y.transpose();
 
-    h1 = x * fnn_module.layer_weights[0] + fnn_module.layer_biases[0];
-    for(auto i=0; i < h1.size(); i++)
-        h1(i) = CondExpGe(h1(i), zero, h1(i), zero);
+    std::vector<ADMatrixXs> layers;
+    layers.push_back(x);
+    layers.push_back(l1);
+    // layers.push_back(n1);
+    layers.push_back(l2);
+    // layers.push_back(n2);
+    layers.push_back(l3);
+    // layers.push_back(n3);
+    layers.push_back(l4);
+    // layers.push_back(n4);
+    layers.push_back(l5);
+    // layers.push_back(n5);
 
-    y = h1 * fnn_module.layer_weights[1] + fnn_module.layer_biases[1];
+    // forward pass
+    
+    std::cout << "Starting forward pass" << std::endl;
+    for (auto i = 1; i < layers.size(); i++)
+    {
+        layers[i] = layers[i - 1] * fnn_module.layer_weights[2 * (i - 1)] + fnn_module.layer_biases[2 * (i - 1)];
+        layers[i] = layer_norm(layers[i]) * fnn_module.layer_weights[2 * (i - 1) + 1] + fnn_module.layer_biases[2 * (i - 1) + 1];
+        for(auto j=0; j < layers[i].size(); j++) {
+            layers[i](j) = CondExpGe(layers[i](j), zero, layers[i](j), zero);
+        }
+    }
+    y = layers.back() * fnn_module.layer_weights.back() + fnn_module.layer_biases.back();
+    std::cout << "Finished forward pass" << std::endl;
+
+    // Perform the forward pass (Linear, layernorm, relu)
+    // 1. Linear
+    // l1 = x * fnn_module.layer_weights[0] + fnn_module.layer_biases[0];
+    // // 2. Layernorm
+    // n1 = layer_norm(l1) * fnn_module.layer_weights[1] + fnn_module.layer_biases[1];
+    // // 3. ReLU
+    // for(auto i=0; i < n1.size(); i++)
+    //     n1(i) = CondExpGe(n1(i), zero, n1(i), zero);
+    
+    // l2 = n1 * fnn_module.layer_weights[2] + fnn_module.layer_biases[2];
+    // // 2. Layernorm
+    // n2 = layer_norm(l2) * fnn_module.layer_weights[3] + fnn_module.layer_biases[3];
+    // // 3. ReLU
+    // for(auto i=0; i < n2.size(); i++)
+    //     n2(i) = CondExpGe(n2(i), zero, n2(i), zero);
+
+    // l3 = n1 * fnn_module.layer_weights[4] + fnn_module.layer_biases[4];
+    // // 2. Layernorm
+    // n3 = layer_norm(l3) * fnn_module.layer_weights[5] + fnn_module.layer_biases[5];
+    // for(auto i=0; i < n3.size(); i++)
+    //     n3(i) = CondExpGe(n3(i), zero, n3(i), zero);
+
+    // l4 = n3 * fnn_module.layer_weights[4] + fnn_module.layer_biases[4];
+    // // 2. Layernorm
+    // n4 = layer_norm(l4) * fnn_module.layer_weights[5] + fnn_module.layer_biases[5];
+
+    // // 3. ReLU
+    // for(auto i=0; i < n1.size(); i++)
+    //     n2(i) = CondExpGe(n2(i), zero, n2(i), zero);
+
+    // y = h1 * fnn_module.layer_weights[1] + fnn_module.layer_biases[1];
 
     // for(auto i=0u; i < fnn_module.layer_weights.size(); i++)
     // {
